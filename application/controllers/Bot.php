859b30fa73891750ef65f88d71aca7e5fc45862f
maulan909 <maulanahilmi909@gmail.com>
2021-07-10 08:47:11 +0700

    membuat configurasi bot telegram

---
+++ application/controllers/Bot.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Bot extends CI_Controller
+{
+    public function index()
+    {
+        $token = "1828680774:AAHIebWcJiKBt0MxpzyWHZheb3ynqDsvFGI";
+        $apiURL = "https://api.telegram.org/bot$token";
+        $update = json_decode(file_get_contents("php://input"), TRUE);
+        $chatID = $update["message"]["chat"]["id"];
+        $message = $update["message"]["text"];
+
+        if (strpos($message, "/start") === 0) {
+
+            file_get_contents($apiURL . "/sendmessage?chat_id=" . $chatID . "&text=Haloo, test webhooks dicoffeean.com.&parse_mode=HTML");
+        }
+    }
+}
